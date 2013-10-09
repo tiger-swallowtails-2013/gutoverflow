6ea3fdcf8dc35fb5cea050ece5c621ba5abf08d4
nathanallen <thenathanator@gmail.com>
2013-10-09 15:16:27 -0700

    initial model level tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
 describe Post do
-  context "text field" do
+  context "text field, invalid input" do
     it "must be at least 1 character long" do
+      expect(Post.create(text: "")).to_not be_valid
     end
   end
 
-  it "belongs to user"
-  it "has many children"
+  it "belongs to user" do
+    user = User.create(full_name:"dave test", password:"asdf12", email:"123@asdf.com")
+    post = Post.create(text: "test")
+    user.posts << post
+    expect(user.posts.last.id).to eq(post.id)
+  end
+
+  it "validates user_id is not null"
+
+  it "has many children (alias responses)" do
+    parent = Post.create(text: "parenttest")
+    childpost1 = Post.create(text: "test1")
+    childpost2 = Post.create(text: "test2")
+    parent.responses << childpost1
+    parent.responses << childpost2
+    expect(parent.responses.all.length).to eq(2)
+  end
   #it "has dependency-destroy on children"
 end
