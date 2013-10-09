194001981f2b314a1ceafbbcfb4641285c291e62
nathanallen <thenathanator@gmail.com>
2013-10-09 15:17:09 -0700

    post migration and model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :topic, class_name: "Post", foreign_key: "post_id"
+  has_many :responses, class_name: "Post", foreign_key: "post_id"
+  validates_length_of :text, minimum: 1
 end
--- db/migrate/20131009191354_create_posts.rb
+++ db/migrate/20131009191354_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.text :text
+      t.belongs_to :post
+      t.belongs_to :user
       t.timestamps
     end
   end
