778bc00f2a5d89ba9da382b8d25e8abad8e3c973
Apprentice <apprentice@dbc-24.local>
2013-10-09 12:34:53 -0700

    create post model and stub out tests

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Post do
+  context "text field" do
+    it "must be at least 1 character long" do
+    end
+  end
+
+  it "belongs to user"
+  it "has many children"
+  #it "has dependency-destroy on children"
+end
