2cfac6bc7691c1dcb47dfeda20db245c46b1c731
Dev Bootcamp <Apprentice@dbc35.local>
2013-10-09 16:28:34 -0700

    validation tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:full_name) }
+  it { should_not allow_value("test@test").for(:email) }
+  it { should allow_value("shouldwork@example.eu").for(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should_not allow_value("!TeD Brogan").for(:full_name) }
+  it { should_not allow_value("jjjj").for(:password) }
+  it { should allow_value("sixormore").for(:password) }
+end
