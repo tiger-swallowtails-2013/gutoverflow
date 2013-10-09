e18faaf21b799ecd5db152dd55aefe550722cbf6
nathanallen <thenathanator@gmail.com>
2013-10-09 16:43:00 -0700

    post model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Post do
   context "text field, invalid input" do
     it "must be at least 1 character long" do
-      expect(Post.create(text: "")).to_not be_valid
+      user = User.create(full_name:"dave test", password:"asdf12", email:"125@asdf.com")
+      expect(Post.create(text: "", user_id: user.id)).to_not be_valid
     end
   end
 
@@ -14,15 +15,23 @@ describe Post do
     expect(user.posts.last.id).to eq(post.id)
   end
 
-  it "validates user_id is not null"
+  it "validates user_id is not null" do
+    post = Post.create(text: "test")
+    expect(post.valid?).to eq(false)
+  end
 
   it "has many children (alias responses)" do
+    user = User.create(full_name:"dave test", password:"asdf12", email:"124@asdf.com")
     parent = Post.create(text: "parenttest")
-    childpost1 = Post.create(text: "test1")
+    parent.user_id = user.id
+    childpost1 = Post.create(text: "test1",)
+    childpost1.user_id = user.id
     childpost2 = Post.create(text: "test2")
+    childpost2.user_id = user.id
     parent.responses << childpost1
     parent.responses << childpost2
     expect(parent.responses.all.length).to eq(2)
   end
-  #it "has dependency-destroy on children"
+
+  pending "has dependency-destroy on children"
 end
